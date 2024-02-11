3ede9aef6d25ac758b9dd64533886258cc02e7b6
Baek <woonn97@gmail.com>
2024-02-11 10:30:12 +0900

    zustand Counter 앱 리셋 기능 추가

--- zustand/src/components/Counter.jsx
+++ zustand/src/components/Counter.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useCounterStore } from "../store";
 
 const Counter = () => {
-  const { count, increment } = useCounterStore();
+  const { count, increment, reset } = useCounterStore();
   return (
     <div>
       <p>{count}</p>
       <button onClick={increment}>one up</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 };
--- zustand/src/store.js
+++ zustand/src/store.js
@@ -3,4 +3,5 @@ import create from "zustand";
 export const useCounterStore = create((set) => ({
   count: 1,
   increment: () => set((state) => ({ count: state.count + 1 })),
+  reset: () => set((state) => ({ count: 1 })),
 }));
