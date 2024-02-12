c898ebc4aaab5bb5fd3860974395bbfff0c18c98
Baek <woonn97@gmail.com>
2024-02-12 11:40:12 +0900

    zustand TodoApp completeTodo 구현

--- zustand/src/store/useTodoStore.js
+++ zustand/src/store/useTodoStore.js
@@ -13,6 +13,17 @@ export const useTodoStore = create((set) => ({
     set((state) => ({
       todos: state.todos.filter((todo) => todo.id !== todoId),
     })),
+  completeTodo: (todoId) =>
+    set((state) => ({
+      todos: state.todos.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            isCompleted: true,
+          };
+        }
+      }),
+    })),
 }));
 
 let id = 0;
