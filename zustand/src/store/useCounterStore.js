4ad85b485a95c7f96200a6379d26f653509ea46c
Baek <woonn97@gmail.com>
2024-02-18 14:40:12 +0900

    useCounter에 devtools 적용

--- zustand/src/store/useCounterStore.js
+++ zustand/src/store/useCounterStore.js
@@ -1,17 +1,19 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist, createJSONStorage, devtools } from "zustand/middleware";
 
 export const useCounterStore = create(
-  persist(
-    (set) => ({
-      count: 1,
-      increment: () => set((state) => ({ count: state.count + 1 })),
-      reset: () => set({ count: 1 }),
-      setNumber: (number) => set({ count: number }),
-    }),
-    {
-      name: "counter-storage",
-      storage: createJSONStorage(() => localStorage),
-    }
+  devtools(
+    persist(
+      (set) => ({
+        count: 1,
+        increment: () => set((state) => ({ count: state.count + 1 })),
+        reset: () => set({ count: 1 }),
+        setNumber: (number) => set({ count: number }),
+      }),
+      {
+        name: "counter-storage",
+        storage: createJSONStorage(() => localStorage),
+      }
+    )
   )
 );
