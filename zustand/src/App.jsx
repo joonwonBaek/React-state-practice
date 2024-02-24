bc90a803de0e92e2e98d6fdaaa3697769ccf378c
Baek <woonn97@gmail.com>
2024-02-24 15:50:12 +0900

    useUserStore App.jsx 에 적용

--- zustand/src/App.jsx
+++ zustand/src/App.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import Counter from "./components/Counter";
 import TodoList from "./components/TodoList";
+import { useUserStore } from "./store/useUserStore";
 
 const App = () => {
+  const { fetch, user } = useUserStore();
+  console.log("user", user);
+  useEffect(() => {
+    fetch(1);
+  }, [fetch]);
   return (
     <div>
       <Counter />
       <TodoList />
+      <p>{user.name}</p>
     </div>
   );
 };
