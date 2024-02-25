8fc3ec99ebcd6181f4c9d382357f6211c40a3527
Baek <woonn97@gmail.com>
2024-02-25 17:51:12 +0900

    reduxsubscribe 적용

--- redux/src/index.tsx
+++ redux/src/index.tsx
@@ -12,15 +12,19 @@ const root = ReactDOM.createRoot(
 
 const store = createStore(counter);
 
-root.render(
-  <React.StrictMode>
-    <App
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-    />
-  </React.StrictMode>
-);
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <App
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      />
+    </React.StrictMode>
+  );
+render();
+
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
